f1db9cb119ea1b10d3946e79736736a98bccb84e
Drew Jensen <drewrjensen@gmail.com>
2020-04-20 18:55:27 -0600

    electron init

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const {app, BrowserWindow} = require("electron");
+// const XMLHttpRequest = require("XMLHttpRequest");
+
+function createWindow() {
+    let window = new BrowserWindow({
+        width: 800,
+        hieght: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    window.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
 function getOwnedGames(steamid) {
     var url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=83BB37ACD8C85F98B6DE61D47B4A6716&steamid=" + steamid + "&include_appinfo=true&format=json";
     var request = new XMLHttpRequest();
@@ -6,4 +23,4 @@ function getOwnedGames(steamid) {
     return JSON.parse(request.responseText);
 }
 
-getOwnedGames(76561198060798137);
+// getOwnedGames(76561198060798137);
